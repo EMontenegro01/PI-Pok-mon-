dd69bfd61ee4c0b2d75051274801d6506ffd08e0
ElianMontenegro <elianmonte@gmail.com>
2023-09-18 19:45:11 -0300

    redux creado

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -8,10 +8,17 @@ const Landing=()=>{
             
             <div className="content">
                 <h1 className="titulo">Pokémon App</h1>
-                <p className="subtitulo">Gotta catch 'em all</p>
+                <h2 className="subtitulo">Gotta catch 'em all</h2>
                 <NavLink to="/home">
-                    <button className="button-home">HOME</button>
+                    <div className="pokeball-container">
+                        <div className="pokeball">
+                            <div className="line"></div>
+                            <div className="circle"></div>
+                            <div className="circle-mini"></div>
+                        </div>
+                    </div>
                 </NavLink>
+                <h3 className="text-pokeball">CLICK THE POKEBALL</h3>
             </div>
         </div>
     )
